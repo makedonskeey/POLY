f766504d4786236cd589f34297dfff3aa1f7463e
Aleksandr <ovchinnikov2.aa@edu.spbstu.ru>
2021-12-02 19:45:49 +0500

    upd1

--- homework1/main.cpp
+++ homework1/main.cpp
@@ -1,17 +1,16 @@
 #include <iostream>
-#include <string>
 #include <fstream>
 #include <cmath>
 
 using namespace std;
 
-long double crossProduct(long double x1, long double y1, long double x2, long double y2){
+float crossProduct(float x1, float y1, float x2, float y2){
     return (x1*y2-y1*x2);
 }
 
-long double distance1(long double x1, long double y1, long double x2, long double y2){
-    long double b = (x1*x2+y1*y2)/sqrt(x1*x1+y1*y1);
-    long double d = (x2*x2+y2*y2) - b*b;
+float distance1(float x1, float y1, float x2, float y2){
+    float b = (x1*x2+y1*y2)/sqrt(x1*x1+y1*y1);
+    float d = (x2*x2+y2*y2) - b*b;
     return (sqrt(d));
 }
 /*double distance2(double x111, double y111, double x222, double y222){
@@ -21,114 +20,54 @@ long double distance1(long double x1, long double y1, long double x2, long doubl
 
 int main() {
 
-    int w = 0;
+    ifstream file("in.txt");
+    file.is_open();
 
-    string path = "in.txt"; //путь к файлу
+    float x0, y0, x, y;
 
-    ifstream fin;
+    file >> x0 >> y0;
 
-    fin.open(path);
-
-    string str;
-
-    while(!fin.eof()) // Когда мы натолкнемся на конец файла, мы выйдем из цикла
-    {
-        fin >> str; //Считываем строки из файла
-        w++;
-    }
-
-    fin.close();
-
-    int u = w;
-
-    fin.open(path);
+    //file.close();
 
 
-    int n = u/2;  // строки
-    int m = 2;  // столбцы
+    float x_r=0,x_l=0,y_r=0,y_l=0;
+    float l1=0, l2=0, var_r=0.0001, var_l=0.0001;
+    float k=0;
 
-    long double vectorCoordinates[n][m];
+   while(!file.eof()){
 
-
-    while(!fin.eof()) // Когда мы натолкнемся на конец файла, мы выйдем из цикла
-    {
-
-        for (int i = 0; i < n; i++)  // Заполняем массив
-        {
-            for (int j = 0; j < m; j++)
-            {
-                fin >> str; //Считываем строки из файла
-
-                const char *c_inp = str.c_str();
-                long double res = atoi(c_inp);
-
-                vectorCoordinates[i][j] = res;
-            }
-        }
-
-    }
-    fin.close();
-
-    long double x = vectorCoordinates[0][0];
-    long double y = vectorCoordinates[0][1];
-
-    long double x_r=0,x_l=0,y_r=0,y_l=0;
-    long double l1=0, l2=0, var=0.00001;
-    long double k=0;
-    /*for (int i = 0; i < n; i++){
-        for (int j = 0; j < m; j++){
-            cout << vectorCoordinates[i][j] << endl;
-        }
-    }*/
-   // cout << vectorCoordinates[0][0] << endl;
-   for (int i = 1; i < n; i++){
-
-        k = crossProduct(x, y, vectorCoordinates[i][0], vectorCoordinates[i][1]);
+        file >> x >> y;
+        k = crossProduct(x0, y0, x, y);
         //cout <<"k="<<k<< endl;
-
-        if (k<0){
-
-            l1 = distance1(x, y, vectorCoordinates[i][0], vectorCoordinates[i][1]);
+        //cout <<"x="<<x<<" y="<<y<< endl;
+        l1 = distance1(x0, y0, x, y);
+        l2 = distance1(x0, y0, x, y);
+        if (k <= 0){
             //cout <<"l1="<<l1<< endl;
-            if (l1>var){
-                var = l1;
-                x_r = vectorCoordinates[i][0];
-                y_r = vectorCoordinates[i][1];
-            }
-        } else if (k>0) {
-
-            l2 = distance1(x, y, vectorCoordinates[i][0], vectorCoordinates[i][1]);
-            //cout <<"l2="<<l2<< endl;
-            if (l2 > var) {
-                var = l2;
-                x_l = vectorCoordinates[i][0];
-                y_l = vectorCoordinates[i][1];
+            if (l1>var_r){
+                var_r = l1;
+                x_r = x;
+                y_r = y;
             }
         } else {
-            x_r = vectorCoordinates[i][0];
-            y_r = vectorCoordinates[i][1];
+            //cout <<"l2="<<l2<< endl;
+            if (l2 > var_l) {
+                var_l = l2;
+                x_l = x;
+                y_l = y;
+            }
+        }
+        if (l2 == 0 and l1 == 0){
+            x_r = x;
+            y_r = y;
         }
     }
 
-    if ((x_r == 0 and y_r == 0) and (x_l != 0 and y_l != 0)){
 
-        cout << "Leftmost: " << x_l << " " << y_l << endl;
-        cout << "Rightmost: " << 0 << " " << 0 << endl;
-    }
-    else if ((x_l == 0 and y_l == 0) and (x_r != 0 and y_r != 0)){
+    file.close();
 
-        cout << "Leftmost: " << 0 << " " << 0 << endl;
-        cout << "Rightmost: " << x_r << " " << y_r << endl;
-    }
-    else if ((x_l==0 and y_l==0) and (x_r==0 and y_r==0)) {
-
-        cout << "Leftmost: " << 0 << " " << 0 << endl;
-        cout << "Rightmost: " << 0 << " " << 0 << endl;
-    }
-    else {
-        cout << "Leftmost: " << x_l << " " << y_l << endl;
-        cout << "Rightmost: " << x_r << " " << y_r << endl;
-    }
+    cout << "Leftmost: " << x_l << " " << y_l << endl;
+    cout << "Rightmost: " << x_r << " " << y_r << endl;
 
     return 0;
 }
