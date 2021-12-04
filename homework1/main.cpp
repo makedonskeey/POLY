ee85fdc03dd26e5741cb79cd48eedcf1fcbc66a9
Aleksandr <ovchinnikov2.aa@edu.spbstu.ru>
2021-12-04 23:51:19 +0500

    upd5

--- homework1/main.cpp
+++ homework1/main.cpp
@@ -4,13 +4,13 @@
 
 using namespace std;
 
-float crossProduct(float x1, float y1, float x2, float y2){
+int crossProduct(int x1, int y1, int x2, int y2){
     return (x1*y2-y1*x2);
 }
 
-float distance1(float x1, float y1, float x2, float y2){
+float distance1(int x1, int y1, int x2, int y2){
     float b = (x1*x2+y1*y2)/sqrt(x1*x1+y1*y1);
-    float d = (x2*x2+y2*y2) - b*b;
+    float d = abs((x2*x2+y2*y2) - b*b);
     return (sqrt(d));
 }
 
@@ -20,11 +20,12 @@ int main() {
     ifstream file("in.txt");
     file.is_open();
 
-    float x0, y0, x, y;
+    int x0, y0, x, y;
 
     file >> x0 >> y0;
 
-    float x_r=0, x_l=0, y_r=0, y_l=0, l1=0, l2=0, var_r=0.00001, var_l=0.00001, k=0;
+    float l1=0, l2=0, var_r=0, var_l=0;
+    int x_r=0, x_l=0, y_r=0, y_l=0, k;
 
    while(!file.eof()){
 
@@ -32,24 +33,25 @@ int main() {
         k = crossProduct(x0, y0, x, y);
         //cout <<"k="<<k<< endl;
         //cout <<"x="<<x<<" y="<<y<< endl;
-        l1 = distance1(x0, y0, x, y);
-        l2 = distance1(x0, y0, x, y);
-        if (k <= 0){
+
+
+        if (k < 0){
+            l1 = distance1(x0, y0, x, y);
             //cout <<"l1="<<l1<< endl;
-            if (l1>var_r){
+            if (l1 >= var_r){
                 var_r = l1;
                 x_r = x;
                 y_r = y;
             }
-        } else {
+        } else if (k > 0) {
+            l2 = distance1(x0, y0, x, y);
             //cout <<"l2="<<l2<< endl;
-            if (l2 > var_l) {
+            if (l2 >= var_l) {
                 var_l = l2;
                 x_l = x;
                 y_l = y;
             }
-        }
-        if (l2 < var_l and l1 < var_r){
+        } else {
             x_r = x;
             y_r = y;
         }
