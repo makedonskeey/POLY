71c66d07387eb10de3753e67de05890724c6f160
Aleksandr <ovchinnikov2.aa@edu.spbstu.ru>
2021-12-05 00:19:16 +0500

    upd6

--- homework1/main.cpp
+++ homework1/main.cpp
@@ -8,7 +8,7 @@ int crossProduct(int x1, int y1, int x2, int y2){
     return (x1*y2-y1*x2);
 }
 
-float distance1(int x1, int y1, int x2, int y2){
+float distance1(float x1, float y1, float x2, float y2){
     float b = (x1*x2+y1*y2)/sqrt(x1*x1+y1*y1);
     float d = (x2*x2+y2*y2) - b*b;
     return (sqrt(d));
@@ -22,7 +22,13 @@ int main() {
 
     int x0, y0, x, y;
 
+
+
+
     file >> x0 >> y0;
+
+    float u0 = static_cast<float>(x0);
+    float v0 = static_cast<float>(y0);
 
     float l1=0, l2=0, var_r=0, var_l=0;
     int x_r=0, x_l=0, y_r=0, y_l=0, k;
@@ -30,12 +36,16 @@ int main() {
    while(!file.eof()){
 
         file >> x >> y;
+
+       float u = static_cast<float>(x);
+       float v = static_cast<float>(y);
+
         k = crossProduct(x0, y0, x, y);
         //cout <<"k="<<k<< endl;
         //cout <<"x="<<x<<" y="<<y<< endl;
 
         if (k < 0){
-            l1 = distance1(x0, y0, x, y);
+            l1 = distance1(u0, v0, u, v);
             //cout <<"l1="<<l1<< endl;
             if (l1 >= var_r){
                 var_r = l1;
@@ -43,7 +53,7 @@ int main() {
                 y_r = y;
             }
         } else if (k > 0) {
-            l2 = distance1(x0, y0, x, y);
+            l2 = distance1(u0, v0, u, v);
             //cout <<"l2="<<l2<< endl;
             if (l2 >= var_l) {
                 var_l = l2;
