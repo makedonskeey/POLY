8a016b78c3ba3810510e8bba4c3252472a9e912a
Aleksandr <ovchinnikov2.aa@edu.spbstu.ru>
2021-12-23 19:43:19 +0500

    last1

--- homework2/main.cpp
+++ homework2/main.cpp
@@ -1,13 +1,11 @@
 #include <iostream>
 #include <cmath>
-#include <string>
 #include <fstream>
-
 
 using namespace std;
 
-int main(int argc, char** argv)
-{
+int main(int argc, char** argv) {
+
     if (argc == 2){
         cout << "1st argument: "<< argv[1] << endl;
     } else {
@@ -15,135 +13,125 @@ int main(int argc, char** argv)
     }
 
     ifstream file(argv[1]);
-    int j=0;
-    auto STR = new double[50];
+
+    double *X = new double [1];
+    double *H = new double [1];
+    double *Y = new double [1];
+
+    int i = 1, k = 0;
+    bool check = false;
 
     if (file.is_open()){
-        //cout << "File is open" << endl;
-        while(!file.eof()){
-            double w;
-            file >> w;
-            STR[j] = w;
-            j = j + 1;
-        }
-    } else {
-        //cout << "File is not open" << endl;
-    }
 
-    double h0 = STR[0], v_x = STR[1], v_y = STR[2];
-    double massiveX[(j-2)/2];
-    for (int m = 2; m<=(j/2); m++){
+        double vx, vy, h0, x, h, t, t1, t2, x1, x2, g = 9.81;
 
-        massiveX[m-1]=STR[2*m-1];
-    }
-    double massiveH[(j-2)/2];
-    for (int u = 2; u<=(j/2); u++){
+        file >> h0;
+        file >> vx >> vy;
 
-        massiveH[u-1]=STR[2*u];
-    }
-    massiveX[0]=0;
-    massiveH[0]=h0;
+        H[0] = h0;
+        Y[0] = h0;
+        X[0] = 0;
 
-    double g=9.81,x1,x2,t,t1,t2;
-    int i = 1;
+        while (check == false) {
 
-    bool check = false;
-    auto Y = new double[10];
-    Y[0] = h0;
-    int k = 0;
+            file >> x >> h;
 
-    while (check == false)
-    {
+            X[i] = x;
+            H[i] = h;
 
-        t=abs((massiveX[i]-massiveX[i-1])/v_x);
+            t = abs( (X[i] - X[i-1] ) / vx);
 
-        if (v_x>0){
-            Y[i]=Y[i-1]+v_y*t-g*t*t/2;
-        } else {
-            Y[i]=Y[i+1]+v_y*t-g*t*t/2;
-        }
-
-        if (Y[i]<=0){
-            cout << i-1 << endl;
-            break;
-        }
-
-        if (Y[i]>massiveH[i]){
-
-            if (v_x>0){
-                i++;
+            if (vx > 0) {
+                Y[i] = Y[i-1] + vy * t - g * t * t / 2;
             } else {
-                i--;
-            }
-            v_y = v_y - g*t;
-
-        } else {
-
-            k=k+1;
-            v_y=v_y-g*t;
-
-            if (k % 2 != 0){
-                v_x = -v_x;
-            } else {
-                v_x = v_x;
+                Y[i] = Y[i+1] + vy * t - g * t * t / 2;
             }
 
-            t1=v_y/g+0.5*sqrt(4*v_y*v_y/(g*g)+8*Y[i]/g);
-            t2=v_y/g-0.5*sqrt(4*v_y*v_y/(g*g)+8*Y[i]/g);
+            if (Y[i] <= 0){
+                cout << i-1 << endl;
+                break;
+            }
 
-            x1 = massiveX[i]+v_x*t1;
-            x2 = massiveX[i]+v_x*t2;
+            if (Y[i] > H[i]){
 
-            if (v_x>0){
-                if (x1>x2){
-                    if (x1<=massiveX[i+1]){
-                        cout << i << endl;
-                        check = true;
+                if (vx > 0){
+                    i++;
+                } else {
+                    i--;
+                }
+                vy = vy - g * t;
+
+            } else {
+
+                k = k + 1;
+                vy = vy - g * t;
+
+                if (k % 2 != 0){
+                    vx = - vx;
+                } else {
+                    vx = vx;
+                }
+
+                t1 = vy / g - 0.5 * sqrt(4 * vy * vy / (g * g) + 8 * Y[i] / g);
+                t2 = vy / g + 0.5 * sqrt(4 * vy * vy / (g * g) + 8 * Y[i] / g);
+
+                x1 = X[i] + vx * t2;
+                x2 = X[i] + vx * t1;
+
+                if (vx > 0){
+                    if (x1 > x2){
+                        if (x1 <= X[i+1]){
+                            cout << i << endl;
+                            check = true;
+                        } else {
+                            i++;
+                        }
                     } else {
-                        i++;
+                        if (x2 <= X[i+1]){
+                            cout << i << endl;
+                            check = true;
+                        } else {
+                            i++;
+                        }
                     }
                 } else {
-                    if (x2<=massiveX[i+1]){
-                        cout << i << endl;
-                        check = true;
+                    if (x1 > x2){
+                        if (x2 >= X[i-1]){
+                            cout << i-1 << endl;
+                            check = true;
+                        } else {
+                            i--;
+                        }
                     } else {
-                        i++;
+                        if (x1 >= X[i-1]){
+                            cout << i-1 << endl;
+                            check = true;
+                        } else {
+                            i--;
+                        }
                     }
                 }
-            } else {
-                if (x1>x2){
-                    if (x2>=massiveX[i-1]){
-                        cout << i-1 << endl;
-                        check = true;
-                    } else {
-                        i--;
-                    }
-                } else {
-                    if (x1>=massiveX[i-1]){
-                        cout << i-1 << endl;
-                        check = true;
-                    } else {
-                        i--;
-                    }
-                }
+                k=0;
             }
-            k=0;
-        }
 
-        if (i<1) {
-            cout << 0;
-            check = true;
-        } else if (i>j/2){
-            cout << i;
-            check = true;
-        } else {
+            if (i < 1) {
+                cout << 0;
+                check = true;
+            }
+
+            if (file.eof()){
+                cout << i;
+                check = true;
+            }
+
 
         }
 
     }
 
+    delete [] X;
+    delete [] H;
     delete [] Y;
-    delete [] STR;
 
     return 0;
 }
